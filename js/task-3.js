9446a20d1cc9331f51f0c679e1f104b5fb2773cb
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-25 02:08:55 +0300

    task 3 finished

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener('input', event => {
+nameInput.addEventListener('input', event => { event.currentTarget.value === "" ? nameOutput.textContent = "Anonymous" :
   nameOutput.textContent = event.currentTarget.value;
 });
