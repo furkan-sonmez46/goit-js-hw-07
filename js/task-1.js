b4f1621de406da81d526f8992ec26bef2c817619
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-24 22:48:23 +0300

    task 1 finished

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,12 @@ let categories = document.querySelectorAll('li.item');
 categories = [...categories];
 
 console.log(`Number of categories : ${categories.length}`);
+
+categories.forEach(item => {
+  const category = item.querySelector('h2').textContent;
+
+  const elementCount = item.querySelectorAll('li').length;
+
+  console.log(`Category: ${category}`);
+  console.log(`Elements: ${elementCount}`);
+});
