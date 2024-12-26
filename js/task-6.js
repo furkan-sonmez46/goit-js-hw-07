4417caf54293beaf46d48eaaf33bd046af783d2f
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-26 23:59:52 +0300

    task 6 complated

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector('.create');
+const destroyButton = document.querySelector('.destroy');
+const input = document.querySelector('.input');
+const boxesContainer = document.querySelector('#boxes')
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.classList.add('boxCSS');
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = input.value;
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
