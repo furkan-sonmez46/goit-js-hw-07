b4ea9962283f5a300edb04ce975728b9c513f580
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-26 21:09:08 +0300

    task 4 complated

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    return console.log('All form fields must be filled in');
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
