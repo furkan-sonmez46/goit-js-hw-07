cdccab7625957bc356c014672fdc7b991c6facc4
Furkan Sonmez <frkn.snmz46@gmail.com>
2024-12-26 22:39:33 +0300

    task 5 complated

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const button = document.querySelector('.change-color');
+const bodyColor = document.querySelector("span.color");
+
+function setRandomHexColor() {
+  const randomHexColor = getRandomHexColor();
+  document.querySelector('body').style.backgroundColor = `${randomHexColor}`;
+
+  bodyColor.textContent = `${randomHexColor}`;
+}
+
+button.addEventListener('click', setRandomHexColor);
+
